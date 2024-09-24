f2f2bc5e6903943489d6379dcfaf3172d555cc0a
Manish Raj Pandey <138515911+Manish3Air@users.noreply.github.com>
2024-09-24 20:08:11 +0530

    Time: 86 ms (83.37%), Space: 98.8 MB (51.26%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    bool f(int i, long long target, vector<int>& nums, vector<vector<int>>& dp) {
+        if (target == 0) return true;
+        if (i == nums.size() || target < 0) return false;
+
+        if (dp[i][target] != -1) return dp[i][target];
+
+        if (f(i + 1, target, nums, dp)) return dp[i][target] = true;
+
+        if (nums[i] <= target) {
+            if (f(i + 1, target - nums[i], nums, dp)) return dp[i][target] = true;
+        }
+
+        return dp[i][target] = false;
+    }
+
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         long long sum = accumulate(nums.begin(), nums.end(), 0LL);
-
-        // If the sum is odd, we cannot partition it into two equal subsets
         if (sum % 2 != 0) return false;
 
-        int target = sum / 2;
+        long long target = sum / 2;
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
 
-        // 1D DP array
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-        // Process each number in nums
-        for (int num : nums) {
-            // Traverse backwards to ensure we don't overwrite results prematurely
-            for (int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
-        }
-
-        // If we can partition the array, there will be a subset with sum equal to target
-        return dp[target];
+        return f(0, target, nums, dp);
     }
 };
