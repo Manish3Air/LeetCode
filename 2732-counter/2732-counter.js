559554a709bc42603a2cb5dda9eabb766865b94a
Manish Raj Pandey <138515911+Manish3Air@users.noreply.github.com>
2024-10-20 12:21:55 +0530

    Time: 57 ms (19.83%), Space: 48.6 MB (82.31%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -3,9 +3,9 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let count = n-1;
+    let count = n;
     return function() {
-        return count+=1;
+        return count++;
     };
 };
 
