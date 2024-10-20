c8180aad2ec53d494ebfe24d774f48ddb1d44b36
Manish Raj Pandey <138515911+Manish3Air@users.noreply.github.com>
2024-10-20 13:12:15 +0530

    Time: 58 ms (19.1%), Space: 48.8 MB (54.62%) - LeetHub

--- 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,24 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map((fn));
+//   const transformedArr = [];
+//   let index = 0;
+//   for (const element of arr) {
+//     transformedArr[index] = fn(element, index);
+//     index++;
+//   }
+//   return transformedArr;
+
+// const transformedArr = [];
+//   arr.forEach((element, index) => {
+//     transformedArr[index] = fn(element, index);
+//   });
+//   return transformedArr;
+
+const transformedArr = [];
+  for(let i=0;i<arr.length; i++){
+    transformedArr[i] = fn(arr[i],i);
+  }
+  return transformedArr;
+
 };
